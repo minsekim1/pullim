61b785c74d820f30a73d854fe62b475a5d5934e9
unknown <tkarnrwl7862@naver.com>
2022-07-09 14:59:34 +0900

    server GET / Node Server is running ... reponse added

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+app.get("/", function (req, res) {
+  res.send("Node server is running ...");
+});
+
 app.use('/images', express.static('uploads'));
 
 app.use('/photo', photoRouter);
