75ed2c9428e4aeadb305b4487de2e85931a7495c
gunhyung <one0374@naver.com>
2022-07-14 10:04:26 +0900

    basic communication complete: using simple-peer package

--- react/src/page/CheckTool.tsx
+++ react/src/page/CheckTool.tsx
@@ -1,6 +1,8 @@
-import React, { useCallback, useEffect, useState } from "react";
-import {Socket, io} from 'socket.io-client';
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { Socket, io } from "socket.io-client";
 import VirtualPhoto from "../components/VirtualPhoto";
+import Peer from "simple-peer";
+
 import {
   BackgroundConfig,
   backgroundImageUrls,
@@ -26,7 +28,7 @@ function CheckTool({
   setCheckedPhotoList,
   isHost,
   userName,
-  meetingNumber
+  meetingNumber,
 }: CheckToolPropsType) {
   const [sourcePlayback, setSourcePlayback] = useState<SourcePlayback>();
   const [backgroundConfig, setBackgroundConfig] = useState<BackgroundConfig>({
@@ -51,14 +53,32 @@ function CheckTool({
   const bodyPix = useBodyPix();
   const { tflite, isSIMDSupported } = useTFLite(segmentationConfig);
   const [isClick, setIsClick] = useState<Boolean>(false);
-  const [second, setSecond] = useState<string>('');
+  const [second, setSecond] = useState<string>("");
+  const [socketData, setSocketData] = useState<Socket>();
+
+  const [stream, setStream] = useState<MediaStream>();
+  const [myId, setMyId] = useState("");
+  const [caller, setCaller] = useState("");
+  const [receivingCall, setReceivingCall] = useState(false);
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [callerSignal, setCallerSignal] = useState<Peer.SignalData | string>(
+    ""
+  );
+
+  const myVideo = useRef() as React.LegacyRef<HTMLVideoElement> &
+    React.MutableRefObject<HTMLVideoElement>;
+  const userVideo = useRef() as React.LegacyRef<HTMLVideoElement> &
+    React.MutableRefObject<HTMLVideoElement>;
+  const connection = useRef<Peer.Instance>();
 
   const changeSecond = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSecond(e.target.value);
-  }
-  
+  };
+
   const createImage = () => {
-    const zoomCanvas = document.querySelector('.single-main-container__canvas') as HTMLCanvasElement;
+    const zoomCanvas = document.querySelector(
+      ".single-main-container__canvas"
+    ) as HTMLCanvasElement;
     const imageBase64 = zoomCanvas.toDataURL("image/png");
     const imageEl = document.createElement("img");
     imageEl.src = imageBase64;
@@ -70,12 +90,15 @@ function CheckTool({
   };
 
   const bringImage = () => {
-    const canvas = document.querySelector("#grid-bg-photo") as HTMLCanvasElement;
+    const canvas = document.querySelector(
+      "#grid-bg-photo"
+    ) as HTMLCanvasElement;
     const img = canvas.toDataURL("image/png");
     return img;
   };
 
-  const setImage = useCallback(async (image: string | undefined) => {
+  const setImage = useCallback(
+    async (image: string | undefined) => {
       if (!image) {
         return;
       }
@@ -96,11 +119,20 @@ function CheckTool({
           };
           setCheckedPhotoList([obj, ...checkedPhotoList]);
         });
-    },[checkedPhotoList, setCheckedPhotoList]);
+    },
+    [checkedPhotoList, setCheckedPhotoList]
+  );
 
   let websocket: Socket | undefined = undefined;
-  const [socketData, setSocketData] = useState<Socket>();
   useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then((stream) => {
+        setStream(stream);
+        console.log(stream);
+        myVideo.current.srcObject = stream;
+      });
+
     if (websocket === undefined) {
       websocket = io(SOCKET_URL, {
         path: "/socket.io", // 서버 path와 일치시켜준다
@@ -109,52 +141,78 @@ function CheckTool({
 
       websocket.on("connect", () => {
         console.info("connect!");
-        if (websocket !== undefined) websocket.emit("join", JSON.stringify({room_id: meetingNumber, isHost,  userName}));
+        if (websocket !== undefined)
+          websocket.emit(
+            "join",
+            JSON.stringify({ room_id: meetingNumber, isHost, userName })
+          );
       });
+
+      websocket.on("getid", (id) => {
+        setMyId(id);
+      });
+
       websocket.on("disconnect", () => console.info("disconnect!"));
       setSocketData(websocket);
     }
-  },[]);
+  }, []);
+  useEffect(() => {
+    if (socketData && stream) {
+      const peer = new Peer({
+        initiator: true,
+        trickle: false,
+        stream: stream,
+      });
+
+      peer.on("signal", (data) => {
+        socketData.emit("caller", {
+          room_id: meetingNumber,
+          signalData: data,
+          from: myId,
+        });
+      });
+      peer.on("stream", (stream) => {
+        console.log(stream);
+        userVideo.current.srcObject = stream;
+      });
+      socketData.on("acceptcall", (signal) => {
+        setCallAccepted(true);
+        peer.signal(signal);
+      });
+      connection.current = peer;
+    }
+  }, [meetingNumber, myId, socketData, stream]);
 
   useEffect(() => {
-    if(isClick && socketData){
-      console.log(socketData);
-      console.log('된다');
-      socketData.emit('checkstart', JSON.stringify({room_id: meetingNumber, isHost,  userName, time: second}));
-    }
-  },[isHost, meetingNumber, second, userName, websocket, isClick, socketData])
-
-
-  useEffect(() =>{
-    if(isClick){
-      setTimeout(() =>{
+    if (isClick) {
+      setTimeout(() => {
         createImage();
-        console.log('useEffect1');
-      },Number(second) * 1000);
+        console.log("useEffect1");
+      }, Number(second) * 1000);
     }
-  },[isClick, second]);
+  }, [isClick, second]);
 
   useEffect(() => {
-    if(isClick && sourcePlayback){
-      setTimeout(() =>{
+    if (isClick && sourcePlayback) {
+      setTimeout(() => {
         const image = bringImage();
         setImage(image);
-        console.log('useEffect2');
+        console.log("useEffect2");
         setIsClick(false);
-      },Number(second) * 1000 + 1000);
+      }, Number(second) * 1000 + 1000);
     }
-  },[sourcePlayback, isClick, setImage, second]);
-    
+  }, [sourcePlayback, isClick, setImage, second]);
 
   const clickCheck = () => {
-    if(second === ''){
-      return alert('숫자를 입력해주세요!');
+    if (second === "") {
+      return alert("숫자를 입력해주세요!");
     }
-    if(Number(second)<1){
-      return alert('숫자는 1이상으로 입력해주세요!');
+    if (Number(second) < 1) {
+      return alert("숫자는 1이상으로 입력해주세요!");
     }
     setIsClick(true);
   };
+
   return (
     <>
       <h4 style={{ color: "black" }}>검사툴</h4>
@@ -169,29 +227,54 @@ function CheckTool({
           overflowY: "auto",
         }}
       >
-
         <label>타이머</label>
-        <input type="number" placeholder="예) 5" onChange={changeSecond} value={second}/>
-        <button onClick={clickCheck}>검사하기</button>
+        <input
+          type="number"
+          placeholder="예) 5"
+          onChange={changeSecond}
+          value={second}
+        />
+        <button onClick={clickCheck}>캡처하기</button>
+        <button>비디오</button>
+        <div style={{ width: "300px", height: "300px", visibility: "hidden" }}>
+          <video
+            ref={myVideo}
+            style={{ width: "100%", height: "100%" }}
+            playsInline
+            autoPlay
+            muted
+          />
+        </div>
+        {callAccepted && (
+          <div style={{ width: "300px", height: "300px" }}>
+            <video
+              style={{ width: "100%", height: "100%" }}
+              playsInline
+              ref={userVideo}
+              autoPlay
+              muted
+            />
+          </div>
+        )}
         <div id="image-container">
-        {checkedPhotoList.length !== 0 &&
-          checkedPhotoList.map((checkedPhoto, i) => (
-            <div
-              key={i}
-              style={{
-                width: "100%",
-                height: "250px",
-                marginBottom: "10px",
-                position: "relative",
-              }}
-            >
-              <img
-                style={{ width: "100%", height: "100%" }}
-                src={checkedPhoto.image}
-                alt={`그리드배경이 들어간 사진 ${i}`}
-              />
-            </div>
-          ))}
+          {checkedPhotoList.length !== 0 &&
+            checkedPhotoList.map((checkedPhoto, i) => (
+              <div
+                key={i}
+                style={{
+                  width: "100%",
+                  height: "250px",
+                  marginBottom: "10px",
+                  position: "relative",
+                }}
+              >
+                <img
+                  style={{ width: "100%", height: "100%" }}
+                  src={checkedPhoto.image}
+                  alt={`그리드배경이 들어간 사진 ${i}`}
+                />
+              </div>
+            ))}
         </div>
         {sourcePlayback && tflite && bodyPix && (
           <VirtualPhoto
