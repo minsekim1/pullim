b3234cf7d454baddde86dbb415b7837b96fb528d
gunhyung <one0374@naver.com>
2022-07-18 09:09:54 +0900

    socket_url fixed

--- react/src/page/Client.tsx
+++ react/src/page/Client.tsx
@@ -8,7 +8,7 @@ import { PostProcessingConfig } from "../core/helpers/postProcessingHelper";
 import useBodyPix from "../core/hooks/useBodyPix";
 import useTFLite from "../core/hooks/useTFLite";
 import VirtualPhoto from "../components/VirtualPhoto";
-const SOCKET_URL = "http://localhost:5002";
+const SOCKET_URL = "http://127.0.0.1:5002";
 
 interface ClientPropsType {
   meetingNumber: string;
