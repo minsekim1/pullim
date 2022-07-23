2cf6d75b5abcb199ebca8a4dc0cc7eece176a800
gunhyung <one0374@naver.com>
2022-07-23 10:36:01 +0900

    client and server replace

--- server/src/index.ts
+++ server/src/index.ts
@@ -120,56 +120,6 @@ io.on("connection", function (socket) {
   });
 });
 
-const httpServer = http.createServer();
-const io = new ServerIO(httpServer);
-
-io.on("connection", function (socket) {
-  // 에러 표시
-  socket.on("error", (error) => l("ERR", "red", error));
-  
-  // 연결수행
-  l("Connect", "green", socket.id);
-
-  // "소켓 아이디 주기"
-  socket.emit("getid", socket.id);
-  
-  socket.on("join", (data) => {
-    const room_id = JSON.parse(data).room_id;
-    l("Join", "green", room_id + "," + socket.id);
-    socket.join(`/${room_id}`);
-    
-  });
-
-  socket.on("caller", (data) => {
-    console.log("됐다");
-    const {room_id} =data;
-    const clients = io.sockets.adapter.rooms.get(`/${room_id}`);
-    let otherUser;
-    for(const user of clients){
-      if(user === socket.id)continue;
-      otherUser = user;
-    }
-    l("caller", "green", room_id + "," + socket.id);
-
-    io.to(otherUser).emit("caller",{
-      signal: data.signalData,
-      from: data.from
-    })
-  });
-
-  // 연결해제
-  socket.on("disconnect", () => {
-    l("Disconnect", "blue", socket.id);
-  });
-
-  socket.on("answerCall", (data) => {
-    io.to(data.to).emit("acceptcall", data.signal);
-  });
-
-});
-
-io.listen(PORT);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
